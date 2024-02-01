7b9a1dcabbad8784b29a128bd3f2405e86ec516f
Jameel Ali <J1m2e3A4i@gmail.com>
2024-01-31 23:09:38 -0400

    NavBar and About me
    Changed text, customised navbar
    created about me page

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
 
 document.getElementById("Load_Button").onclick = function() {
-	console.log("Button Pressed")
-	document.getElementById("Main_Video").play()
-	document.getElementById("Load_Button").remove()
-	document.getElementById("vid_hider").remove()
+	
 }
