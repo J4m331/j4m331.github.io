da43b0012ddedd6ea7309ad5d86c96ec7573c071
Jameel Ali <80857027+J4m331@users.noreply.github.com>
2024-08-11 02:23:19 -0400

    Update countrytest.js

--- Projects/HTML Applications/countrytest.js
+++ Projects/HTML Applications/countrytest.js
@@ -1,11 +1,8 @@
 //API: https://restcountries.com/v3.1/all
 
-console.log("Yee");
 async function fetchData() {
     const responce = await fetch("https://restcountries.com/v3.1/all");
     const data = await responce.json();
     
     console.log(data);
 }
-
-fetchData();
