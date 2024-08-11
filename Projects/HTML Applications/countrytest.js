6ab64b92c159eaa7826ee87bd38c57b23e328781
Jameel Ali <J1m2e3A4i@gmail.com>
2024-08-11 15:07:27 -0400

    nation flag attempt

--- Projects/HTML Applications/countrytest.js
+++ Projects/HTML Applications/countrytest.js
@@ -1,8 +1,21 @@
 //API: https://restcountries.com/v3.1/all
 
-async function fetchData() {
-    const responce = await fetch("https://restcountries.com/v3.1/all");
-    const data = await responce.json();
+async function fetchData(string) {
+    var responce = await fetch("https://restcountries.com/v3.1/all");
+    var data = await responce.json();
     
-    console.log(data);
+    //console.log(data);
+    search(data,string);
 }
+
+function search(data,string){
+    for(d of data){
+        string = string.toUpperCase()
+        if(d.cca2 == string){
+            console.log(d.flag);
+        }
+    }
+}
+
+
+fetchData("dz");
