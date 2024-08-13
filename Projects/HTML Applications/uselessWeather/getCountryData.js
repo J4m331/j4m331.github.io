4c60b1f133962447db4b4d7742697e50368c35b8
Jameel Ali <80857027+J4m331@users.noreply.github.com>
2024-08-13 00:06:00 -0400

    Flag and local Location TIme

--- Projects/HTML Applications/uselessWeather/getCountryData.js
+++ Projects/HTML Applications/uselessWeather/getCountryData.js
@@ -3,18 +3,21 @@
 async function fetchData() {
     const responce = await fetch("https://restcountries.com/v3.1/all");
     const data = await responce.json();
+
+    //for(let i = 0; i < 255; i++){
+    //    console.log(i + " : " + data[i].name.official);
+    //}
+    console.log(getFlag(data,"tt"));
 }
 
-export function getFlag(data,code){
+function getFlag(data,code){
     for(let d of data){
-        code = string.toUpperCase();
-        if(d.cca2 == string){
+        code = code.toUpperCase();
+        if(d.cca2 == code){
             console.log(d.flag);
+            return d.flag;
         }
     }
-
-    return d.flag;
 }
-
 
 fetchData();
